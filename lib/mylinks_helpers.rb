55946e9e995bbec0fe065f01a01975449465336e
baob <coder@onesandthrees.com>
2015-07-09 22:29:19 +0100

    yaml helpers

--- lib/mylinks_helpers.rb
+++ lib/mylinks_helpers.rb
@@ -1,3 +1,24 @@
+require 'yaml'
+
 module MylinksHelpers
+
+  def yaml_load_files(files_glob)
+    full_files_glob = File.join(app_root, files_glob, '*')
+
+    Dir[full_files_glob].inject({}) do |result, file|
+      key = File.basename(file).split('.').first.to_sym
+      result[key] = YAML.load_file(file)
+      result
+    end
+  end
+
+  def yaml_load_file(file)
+    full_file = File.join(app_root, file)
+    YAML.load_file(full_file)
+  end
+
+  def app_root
+    app_root = File.expand_path('../..', __FILE__)
+  end
 
 end
