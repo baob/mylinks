60ef313fb32b05b5d0766a02328ded56eb985f53
baob <coder@onesandthrees.com>
2015-07-13 10:12:25 +0100

    extend timeout for middleman build

--- spec/middleman_build_spec.rb
+++ spec/middleman_build_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'middleman build', type: :aruba do
 
   specify 'runs without error' do
-    run_simple 'middleman build'
+    run_simple('middleman build', true, 6) # 6 second timeout
   end
 
 end
